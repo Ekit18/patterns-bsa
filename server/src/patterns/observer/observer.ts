614076e40f39ed55c82a1109b3d44d4b21e0d290
evgen <evgenkitan@gmail.com>
2023-08-02 16:55:20 +0300

    patterns-2: + prototype, type for its subscribers

---
+++ server/src/patterns/observer/observer.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+import { ISubscriber } from '../../common/types/observerType';
+import * as  fs from 'fs';
+
+class Publisher {
+    private observers: ISubscriber[] = [];
+
+    subscribe(observer: ISubscriber): void {
+        this.observers.push(observer);
+    }
+
+    unsubscribe(deletedSubscriber: ISubscriber): void {
+        this.observers = this.observers.filter((subscriber) => subscriber !== deletedSubscriber);
+    }
+
+    notifySubscribers(observer: ISubscriber, data: any): void {
+        observer.notify(data);
+    }
+}
+
+export const observer = new Publisher();
+
+export const logData: ISubscriber = {
+    notify: (data) => {
+        fs.appendFileSync('./logs.txt', `${JSON.stringify(data)}\n`);
+    },
+};
+
+export const errorData: ISubscriber = {
+    notify: (data) => {
+        console.log(data);
+    },
+};
+
+observer.subscribe(logData);
+observer.subscribe(errorData);
